f46999b26574cfe6816253b5285e18173e74fd51
bjohnson <bkj.322@gmail.com>
2018-06-20 10:56:05 -0400

    adding synthetic data

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ from scipy.sparse.csgraph import minimum_spanning_tree
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inpath', type=str, default="X.tsv")
+    parser.add_argument('--inpath', type=str, default="X2.tsv")
     parser.add_argument('--outpath', type=str)
+    parser.add_argument('--two-d', action="store_true")
     return parser.parse_args()
 
 
@@ -38,12 +39,7 @@ def scan_stat(g, offset=0):
     return np.array(R)
 
 
-def scan_stat_2d(g):
-    return np.vstack([scan_stat(g, offset=offset + 1) for offset in g.nodes])
-
-
-def compute_Z_point(R, g):
-    """ Normalize R, assuming permutation distribution """
+def compute_Z(R, g):
     nE     = g.number_of_edges()
     n      = g.number_of_nodes()
     deg_sq = (np.array(dict(g.degree).values()) ** 2).sum()
@@ -55,6 +51,10 @@ def compute_Z_point(R, g):
     p3_tt  = 4 * tt * (n - tt) * (tt - 1) * (n - tt - 1) / (n * (n - 1) * (n - 2) * (n - 3))
     A_tt   = (p1_tt - 2 * p2_tt + p3_tt) * nE + (p2_tt - p3_tt) * deg_sq + p3_tt * (nE ** 2)
     return (mu_t - R) / np.sqrt(A_tt - (mu_t ** 2) + 1e-7)
+
+
+def scan_stat_2d(g):
+    return np.vstack([scan_stat(g, offset=offset + 1) for offset in g.nodes])
 
 
 def compute_Z_2d(R_2d, g):
@@ -78,6 +78,14 @@ def random_stat(g, num_nodes):
     return len([[e for e in g.edges(n) if e[1] not in nodes] for n in nodes])
 
 
+def melt_array(x):
+    df = pd.DataFrame(x)
+    df['start'] = np.arange(df.shape[0])
+    df = pd.melt(df, id_vars='start')
+    df.columns = ('start', 'end', 'value')
+    return df
+
+
 # --
 # Run
 
@@ -97,11 +105,18 @@ if __name__ == "__main__":
     # --
     # Run changepoint detection
     
-    R = scan_stat(g)
-    Z = compute_Z(R, g)
+    if not args.two_d:
+        R = scan_stat(g)
+        Z = compute_Z(R, g)
+        
+        out_df = pd.DataFrame({"R" : R, "Z" : Z})
+        
+    else:
+        R = scan_stat_2d(g)
+        Z = compute_Z_2d(R, g)
+        
+        out_df = melt_array(R)
+        out_df.columns = ('start', 'end', 'R')
+        out_df['Z'] = melt_array(Z)['value']
     
-    # --
-    # Write results
-    
-    df = pd.DataFrame({"R" : R, "Z" : Z})
-    df.to_csv(args.outpath, sep='\t', index=False)
+    out_df.to_csv(args.outpath, sep='\t', index=False)
